7127d1655e25488d61c3b01274f173b83cd99d37
hkooost <djzimniy@gmail.com>
2024-07-30 09:41:09 +0300

    feat: remove theme status

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,9 +1,7 @@
 const themes = document.querySelectorAll('[name="theme"]');
-const themeBar = document.querySelector('.current-theme');
 
 let saveTheme = (theme) => {
   localStorage.setItem("theme", theme);
-  themeBar.innerText = theme;
 }
 themes.forEach((currentTheme) => {
   currentTheme.addEventListener("click", () => {
@@ -12,7 +10,6 @@ themes.forEach((currentTheme) => {
   if (window.matchMedia('(prefers-color-scheme: dark)')) {
     if (currentTheme.classList[1] === 'dark') {
       currentTheme.checked = true;
-      themeBar.innerText = currentTheme.classList[1];
     }
   }
 })
@@ -21,7 +18,6 @@ let loadTheme = () => {
   themes.forEach((currentTheme) => {
     if (currentTheme.classList[1] === savedTheme) {
       currentTheme.checked = true;
-      themeBar.innerText = savedTheme;
     }
   })
 }
